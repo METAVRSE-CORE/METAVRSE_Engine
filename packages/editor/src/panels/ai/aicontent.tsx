9de2cd7d7ee41bd26a2339fae8718f7ed2edc844
Abhi Patel <a.patel@bsdxr.com>
2024-12-09 13:54:46 -0500

    Add aicontent.tsx add text box for AI prompt.

--- packages/editor/src/panels/ai/aicontent.tsx
+++ packages/editor/src/panels/ai/aicontent.tsx
@@ -23,29 +23,13 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import { PanelDragContainer, PanelTitle } from '@ir-engine/ui/src/components/editor/layout/Panel'
-import { TabData } from 'rc-dock'
+import TextArea from '@ir-engine/ui/src/primitives/tailwind/TextArea'
 import React from 'react'
-import { useTranslation } from 'react-i18next'
-import { AiContent } from './aicontent'
 
-const AiPanelTitle = () => {
-  const { t } = useTranslation()
-
+export const AiContent = () => {
   return (
-    <div>
-      <PanelDragContainer dataTestId="assets-panel-tab">
-        <PanelTitle>
-          <span>{t('editor:tabs.ai')}</span>
-        </PanelTitle>
-      </PanelDragContainer>
+    <div className="ai-content h-full w-full">
+      <TextArea className="text-white" value={'Abhi'} />
     </div>
   )
-}
-
-export const AiPanelTab: TabData = {
-  id: 'aiPanel',
-  closable: true,
-  title: <AiPanelTitle />,
-  content: <AiContent />
 }
